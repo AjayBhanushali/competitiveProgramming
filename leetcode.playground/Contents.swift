60a7688e8d6f8337e165abaabd480e54b95c9336
Ajay Bhanushali <ajay.bhanushali@upgrad.com>
2021-09-01 07:08:14 +0530

    majority element
    https://leetcode.com/problems/majority-element/

--- leetcode.playground/Contents.swift
+++ leetcode.playground/Contents.swift
@@ -1,3 +1,20 @@
+//https://leetcode.com/problems/majority-element/
+func majorityElement(_ nums: [Int]) -> Int {
+    let maxCount = nums.count/2
+    var counts: [Int: Int] = [:]
+    for num in nums {
+        counts[num] = (counts[num] ?? 0) + 1
+        if counts[num]! > maxCount {
+            return num
+        }
+    }
+    return 0
+}
+
+majorityElement([2,3,3])
+majorityElement([2,3,3,4,4,7,7,7,4,4,4,4,4,4])
+
+
 func maxProfit(_ prices: [Int]) -> Int {
     guard var min = prices.first else {
         return 0
@@ -42,7 +59,6 @@ func maxSubArray(_ nums: [Int]) -> Int {
     for _ in 0..<nums.count {
         for _ in 0..<nums.count-newLength {
             let sum = nums[startIndex...(startIndex+newLength)].reduce(0, +)
-            
             if sum > max {
                 thatArray = nums[startIndex...(startIndex+newLength)]
                 max = sum
